d5a0ee0fa0b0df76458b75d7791343c387294e57
finchstier <yomi@eod.tv>
2015-05-15 13:17:05 +0100

    Update Domain.java

--- src/main/java/com/eod/digital/model/Domain.java
+++ src/main/java/com/eod/digital/model/Domain.java
@@ -1,4 +1,15 @@
 package com.eod.digital.model;
 
 public class Domain {
+  private final String name;
+  private final String url;
+  private final String token;
+  private final String description;
+  
+  private Domain(Builder builder) {
+      this.name = builder.name;
+      this.url = builder.url;
+      this.token = builder.token;
+      this.description = builder.description;
+  }
 }
